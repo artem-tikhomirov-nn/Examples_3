e705ff769f9bfd0b50096b37636223ae21c365b3
Artem Tikhomirov <artem.tikhomirov.nn@gmail.com>
2022-12-13 13:33:37 +0300

    add Example_019

--- Example_019/Program.cs
+++ Example_019/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+Boolean cont = true;
+while (cont)
+{
+    string text = Console.ReadLine();
+    char[] revtext = text.ToCharArray();
+    if (revtext.Length - 1 < 5)
+    {
+        Array.Reverse(revtext);
+        string finaltext = new string(revtext);
+        string result = text == finaltext ? "Палиндром" : "Не палиндром";
+        Console.WriteLine(result);
+        Console.WriteLine("Продолжим(Yes/No) ?");
+        string next = Console.ReadLine();
+        cont = next.ToLower() == "yes" ? true : false;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не верное значение, необходимо пятизначное.");
+    }
+}
